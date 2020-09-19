8b494fd899977f361eed6f63931bfc42df0e247d
bodia <v.bodyansky@yandex.ru>
2020-09-19 22:44:53 +1000

    Add JSON format config.

--- Singleton.cs
+++ Singleton.cs
@@ -5,71 +5,86 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace Singleton {
 
     class Config {
         private static Config _Instance;
-        private string _IP, _Login, _Password;
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "login")]
+        public string _Login { get; private set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "ip")]
+        public string _IP { get; private set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "passwd")]
+        public string _Password { get; private set; }
+
+        private List<string> Properties = new List<string> { "login", "ip", "passwd" };
+
         private Config() { }
 
         public static Config GetInstance() {
             if (_Instance == null) {
-                _Instance = new Config();                
-            }   
+                _Instance = new Config();
+            }
             return _Instance;
         }
 
-        private static bool IsValidIP(List<String> IP) {
+        private bool IsValidIP(string IP) {
             try {
-                return IP.Where(x => Int32.Parse(x) <= 255).ToList().Count == 4;
-            } catch {
+                return IP
+                    .Split('.')
+                    .ToList()
+                    .Where(x => Int32.Parse(x) >= 0)
+                    .Count() == 4;
+            }
+            catch {
                 return false;
             }
         }
 
         public void Update(string IP, string Login, string Password) {
-            _IP = IsValidIP(IP.Split('.').ToList()) ? IP : throw new Exception("Не корректный IP!.");
+            _IP = IsValidIP(IP) ? IP : throw new Exception("Не корректный IP!.");
             _Login = Login;
             _Password = Password;
         }
 
         public void GetConfig() {
-            Console.WriteLine($"IP = {_IP} \nLogin = {_Login} \nPassword = {_Password}");
+            if (_IP == null || _Password == null || _Login == null)
+                Console.WriteLine("Конфиг пустой.");
+            else
+                Console.WriteLine($"IP = {_IP} \nLogin = {_Login} \nPassword = {_Password}");
         }
 
         public void UpdateFromFile(string path) {
-            List<string> param = new List<string>();
+            JObject obj = JObject.Parse(File.ReadAllText(path));
             try {
-                using (StreamReader Out = new StreamReader(path)) {
-                    string line;
-                    while ((line = Out.ReadLine()) != null) {
-                        param.Add(line.Substring(line.IndexOf('=') + 2));
-                    }
-                }
-                if (param.Count == 3) {
-                    _IP = IsValidIP(param[0].Split('.').ToList())
-                        ? param[0]
-                        : throw new FormatException("Не корректный IP!");
-                    _Login = param[1];
-                    _Password = param[2];
-                }
-                else {
-                    throw new FormatException("Неверный формат параметров!");
-                }
+                obj.Properties().ToList().ForEach(item => {
+                    if (Properties.IndexOf(item.Name) == -1)
+                        throw new FormatException($"Не удалось найти свойство {item.Name}. Данные не были обновлены");
+                });
+
+                _IP = IsValidIP(obj.GetValue("ip").ToString())
+                    ? obj.GetValue("ip").ToString()
+                    : throw new ArgumentException("Не корректный IP!");
+
+                _Login = obj.GetValue("login").ToString();
+                _Password = obj.GetValue("passwd").ToString();
             }
             catch(FormatException ex) {
-                Console.WriteLine($"{ex.Message} Данные не были обновлены.");
+                Console.WriteLine(ex.Message);
             }
-            catch(FileNotFoundException ex) {
-                Console.WriteLine($"{ex.Message} Данные не были обновлены.");
+            catch(ArgumentException ex) {
+                Console.WriteLine(ex.Message);
             }
         }
 
         public void WriteToFile(string path) {
-            using (StreamWriter In = new StreamWriter(path)) {
-                In.WriteLine($"IP = {_IP} \nLogin = {_Login} \nPassword = {_Password}");
-            }
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
         }
     }
 }
